45d6ba7f9e928bf7abe11f4c31a42ebb5de41d65
polovnevdd <116808735+polovnevdd@users.noreply.github.com>
2024-12-03 22:11:07 +0100

    Delete Lesson1 directory

--- Lesson1/src/Animals.java
+++
@@ -1,8 +0,0 @@
-public abstract class Animals {
-
-    public abstract void run(int distance);
-    public abstract void swim(int distance);
-    public static int getAnimalsCount(){
-        return Dog.getDogCount() + Cat.getCatCount();
-    }
-}
