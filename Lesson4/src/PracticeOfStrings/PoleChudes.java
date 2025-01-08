d535b10dc9ccc44ba5f015a78b141f9f703a635b
polovnevdd <116808735+polovnevdd@users.noreply.github.com>
2025-01-08 16:12:48 +0100

    Add files via upload

---
+++ Lesson4/src/PracticeOfStrings/PoleChudes.java
@@ -0,0 +1,36 @@
+package PracticeOfStrings;
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class PoleChudes {
+    public static void main(String[] args) {
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli",
+                "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango",
+                "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper",
+                "pineapple", "pumpkin", "potato"};
+
+        Random rand = new Random();
+        String secretWord = words[rand.nextInt(24)];
+        System.out.println("Введите ваше слово: ");
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String usersWord = sc.nextLine();
+            if (secretWord.equals(usersWord)) {
+                System.out.println("Ты угадал!");
+                break;
+            }
+            for (int i = 0; i < 15; i++) {
+                if (i < secretWord.length() && i < usersWord.length() && secretWord.charAt(i) == usersWord.charAt(i)) {
+                    System.out.print(secretWord.charAt(i));
+                } else {
+                    System.out.print("#");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
