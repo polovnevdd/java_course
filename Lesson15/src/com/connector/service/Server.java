abb1421e4c4eeba1d4827d542d649ed2f7fcc195
polovnevdd <polovnevdd@gmail.com>
2025-03-18 23:41:14 +0200

    New commit

--- Lesson15/src/com/connector/service/Server.java
+++ Lesson15/src/com/connector/service/Server.java
@@ -10,31 +10,18 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Server {
-    public void serverConnector(){
+    public void serverConnector() {
         int port = 8081;
-        try (ServerSocket serverSocket = new ServerSocket(port)){
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Сервер запущено. Очікуємо клієнта...");
             Socket socket = serverSocket.accept();
             System.out.println("Клієнт підключився!");
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(),true);
-            ServerManager serverManager = new ServerManager();
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+            Server server = new Server();
             String line;
-            while ((line = input.readLine()) != null){
-                if(serverManager.PerevirkaNaKozaka(line) != true) {
-                    output.println("Що таке паляниця?");
-                    line = input.readLine();
-                    if(line.toLowerCase().equals("хлеб")){
-                        LocalDateTime now = LocalDateTime.now();
-                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-                        String formattedDateTime = now.format(formatter);
-                        output.println("Дякую, до побачення!  " + formattedDateTime);
-                        break;
-                    }else{
-                        System.out.println(line);
-                        break;
-                    }
-                }
+            while ((line = input.readLine()) != null) {
+                server.isClientNotRussion(line, input, output);
                 output.println(line);
                 System.out.println("Повідомлення від клієнта: " + line);
             }
@@ -43,11 +30,24 @@ public class Server {
             socket.close();
             System.out.println("Клієнт відключився!");
 
-        }catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    private void isClientNotRussion(){
 
+    private void isClientNotRussion(String line, BufferedReader input, PrintWriter output) throws IOException {
+        ServerManager serverManager = new ServerManager();
+        if (serverManager.PerevirkaNaKozaka(line) != true) {
+            output.println("Що таке паляниця?");
+            line = input.readLine();
+            if (line.toLowerCase().equals("хлеб")) {
+                LocalDateTime now = LocalDateTime.now();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                String formattedDateTime = now.format(formatter);
+                output.println("Дякую, до побачення!  " + formattedDateTime);
+            } else {
+                System.out.println(line);
+            }
+        }
     }
 }
